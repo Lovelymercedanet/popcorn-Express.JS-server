272f247127f568e7949034ef1677691698343237
Lovelymercedanet <ruslantooyoung@gmail.com>
2025-02-14 18:11:39 +0500

    cors configuration

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const apiRoutes = require("./routes/movies");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://popcorn-express-js-server.onrender.com'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
 
 // Подключение к MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
