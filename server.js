8a6d543fe38b1cbd5f784de2392bfb23595059ee
Lovelymercedanet <ruslantooyoung@gmail.com>
2025-02-14 18:19:03 +0500

    remove cors configurations

--- server.js
+++ server.js
@@ -8,11 +8,7 @@ const apiRoutes = require("./routes/movies");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: ['http://localhost:3000', 'https://popcorn-express-js-server.onrender.com'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-}));
+app.use(cors());
 
 // Подключение к MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
